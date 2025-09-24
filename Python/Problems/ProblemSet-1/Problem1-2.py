48d4e8ecfeb0dff0fbf41272245b67c6433058a6
hafsafatima333 <fatimahafsa365@gmail.com>
2025-09-24 22:25:29 +0500

    Updated Code

--- Python/Problems/ProblemSet-1/Problem1-2.py
+++ Python/Problems/ProblemSet-1/Problem1-2.py
@@ -57,12 +57,9 @@ class LinkedList:
 
     
 l = LinkedList()
-l.push(1)
-l.push(2)
-l.push(2)
-l.push(2)
-l.push(3)
-l.push(4)
+
+for i in [1, 2, 2, 2, 3, 4]:
+    l.push(i)
 
 print(l)
 print(l.count())
